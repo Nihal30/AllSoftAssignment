b094ecdbd71fd54fae5b65d8f27906227bcbe8f9
Nihal <nihal301298@gmail.com>
2025-03-27 12:33:10 +0530

    bootstrap added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,7 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
-import {
-  TextField,
-  Button,
-  MenuItem,
-  Autocomplete,
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-} from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import LOGO from "../assets/logo.jpeg";
+import { Navbar, Container, Form, Button, Row, Col } from "react-bootstrap";
 
 const categories = ["Personal", "Professional"];
 const predefinedTags = ["Invoice", "Report", "Contract", "Receipt"];
@@ -31,133 +19,92 @@ const FileSearch = ({ onSearch }) => {
   };
 
   return (
-    <AppBar position="static" sx={{ bgcolor: "#36454F", padding: "10px" }}>
-      <Toolbar
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box>
+    <Navbar bg="success" variant="dark" expand="lg" className="p-3">
+      <Container className="d-flex justify-content-between align-items-center">
+        {/* Logo */}
+        <div>
           <img src={LOGO} alt="LOGO" style={{ width: 120, borderRadius: 120 }} />
-        </Box>
-        <Box
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            width: "100%",
-            maxWidth: "600px",
-          }}
-        >
-          {/* Dropdowns in a Single Row */}
-          <Box sx={{ display: "flex", gap: "10px" }}>
-            <Controller
-              name="category"
-              control={control}
-              rules={{ required: "Category is required" }}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  select
-                  label="Category"
-                  error={!!errors.category}
-                  helperText={errors.category?.message}
-                  sx={{
-                    backgroundColor: "white",
-                    borderRadius: "5px",
-                    minWidth: 260,
-                  }}
-                >
-                  {categories.map((option) => (
-                    <MenuItem key={option} value={option}>
-                      {option}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              )}
-            />
-            <Controller
-              name="tags"
-              control={control}
-              render={({ field }) => (
-                <Autocomplete
-                  {...field}
-                  multiple
-                  options={predefinedTags}
-                  freeSolo
-                  onChange={(_, newValue) => setValue("tags", newValue)}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Tags"
-                      fullWidth
-                      sx={{
-                        backgroundColor: "white",
-                        borderRadius: "5px",
-                        minWidth: 260,
-                      }}
+        </div>
+
+        {/* Form */}
+        <Form onSubmit={handleSubmit(onSubmit)} className="w-100" style={{ maxWidth: "600px" }}>
+          {/* Dropdowns Row */}
+          <Row className="mb-2">
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Category</Form.Label>
+                <Controller
+                  name="category"
+                  control={control}
+                  rules={{ required: "Category is required" }}
+                  render={({ field }) => (
+                    <Form.Select {...field} isInvalid={!!errors.category}>
+                      <option value="">Select Category</option>
+                      {categories.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Form.Select>
+                  )}
+                />
+                <Form.Control.Feedback type="invalid">{errors.category?.message}</Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>Tags</Form.Label>
+                <Controller
+                  name="tags"
+                  control={control}
+                  render={({ field }) => (
+                    <Form.Control
+                      {...field}
+                      type="text"
+                      placeholder="Enter tags"
+                      onChange={(e) => setValue("tags", e.target.value.split(","))}
                     />
                   )}
                 />
-              )}
-            />
-          </Box>
-          {/* Date Pickers in a Single Row */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Box sx={{ display: "flex", gap: "10px" }}>
-              <Controller
-                name="fromDate"
-                control={control}
-                render={({ field }) => (
-                  <DatePicker
-                    {...field}
-                    label="From Date"
-                    onChange={(newValue) => setValue("fromDate", newValue)}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        fullWidth
-                        sx={{ backgroundColor: "white", borderRadius: "5px" }}
-                      />
-                    )}
-                  />
-                )}
-              />
-              <Controller
-                name="toDate"
-                control={control}
-                render={({ field }) => (
-                  <DatePicker
-                    {...field}
-                    label="To Date"
-                    onChange={(newValue) => setValue("toDate", newValue)}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        fullWidth
-                        sx={{ backgroundColor: "white", borderRadius: "5px" }}
-                      />
-                    )}
-                  />
-                )}
-              />
-            </Box>
-          </LocalizationProvider>
+              </Form.Group>
+            </Col>
+          </Row>
+
+          {/* Date Pickers Row */}
+          <Row className="mb-2" >
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>From Date</Form.Label>
+                <Controller
+                  name="fromDate"
+                  control={control}
+                  render={({ field }) => (
+                    <Form.Control {...field} type="date" onChange={(e) => setValue("fromDate", e.target.value)} />
+                  )}
+                />
+              </Form.Group>
+            </Col>
+            <Col md={6}>
+              <Form.Group>
+                <Form.Label>To Date</Form.Label>
+                <Controller
+                  name="toDate"
+                  control={control}
+                  render={({ field }) => (
+                    <Form.Control {...field} type="date" onChange={(e) => setValue("toDate", e.target.value)} />
+                  )}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+
           {/* Search Button */}
-          <Button
-            type="submit"
-            variant="contained"
-            sx={{ bgcolor: "#000", color: "#fff", maxWidth: "88%" }}
-          >
+          <Button type="submit" variant="dark" className="w-100">
             Search
           </Button>
-        </Box>
-      </Toolbar>
-    </AppBar>
+        </Form>
+      </Container>
+    </Navbar>
   );
 };
 
