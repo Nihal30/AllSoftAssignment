292f87680b5f494d2a754caf80c0cd9306ff983f
Nihal <nihal301298@gmail.com>
2025-03-27 15:32:18 +0530

    token validation added

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Form from "../components/Form";
 import FileResults from "../components/FileResults";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token"); // Check token in localStorage
+    // console.log('token', token)
+    if (!token) {
+      navigate("/"); // Redirect to login if token is missing
+    }
+  }, [navigate]);
+
   return (
     <div>
       <NavBar />
